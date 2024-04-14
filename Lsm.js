1f89d319450e40cb6443309e47f4ca962906429e
ODALDMAWPKJD <128695507+ODALDMAWPKJD@users.noreply.github.com>
2024-04-14 20:30:32 +0800

    Add files via upload

---
+++ Lsm.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('BTN');
+    const closebtn = document.getElementById('cls-btn');
+    const container = document.querySelector('.main-container');
+    let isOpen = true;
+    let isClose = true;
+    button.addEventListener('click', isToggleContainer);
+    closebtn.addEventListener('click',close);
+    
+function isToggleContainer(){
+    if(isOpen){
+        showContainer();
+        buttonHideShow()
+    }
+    else{
+        closeContainer();
+        button.style.top = "50%";
+        isOpen = true;
+    }
+}
+function closeContainer(){
+        container.style.display = 'none';
+    }
+function showContainer() {
+        container.style.display = 'block';
+        closebtn.style.display = "block"
+    }
+function buttonHideShow(){
+    button.style.display = 'none';
+    container.style.display = 'block';
+    closebtn.style.display = "block";
+}
+function close(){
+if(isClose){
+    closeContainer();
+    isClose = false;
+    document.getElementById('cls-btn').innerText = "OPEN";
+}
+else{
+    showContainer()
+    isClose = true;
+    document.getElementById('cls-btn').innerText = "ClOSE";
+}
+}})
